314ad82f3ba802bf89371caa3c63145df40fec20
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-07-17 13:17:54 +0530

    stop the workflows

--- employee/scheduler.py
+++ employee/scheduler.py
@@ -181,7 +181,6 @@ def send_probation_end_notifications():
         send_probation_email(employee, 'probation_notification.html', context)
 
 
-
 def send_probation_email(employee, template_name, context):
 
     subject = f"Probation Period Notification: {employee.employee_id.get_full_name()}"
@@ -216,6 +215,36 @@ def send_probation_email(employee, template_name, context):
     #     fail_silently=False
     # )
 
+
+def send_contract_end_notification():
+    from django.utils import timezone
+    from employee.models import EmployeeWorkInformation
+
+    today = timezone.now().date()
+
+    ending_today = EmployeeWorkInformation.objects.filter(
+        contract_end_date=today,
+        employee_id__is_active=True
+    )
+    # new contract date > last contrat date
+    # print("Contract",ending_today)
+
+
+def send_birthday_in_this_month_notification():
+    from django.utils import timezone
+    from employee.models import Employee
+
+    today = timezone.now().date()
+    current_month = today.month
+
+    birthday_list = Employee.objects.filter(
+        dob__month=current_month,
+
+    )
+    # only once
+    # print("Birhtday" , birthday_list)
+
+
 if not any(
     cmd in sys.argv
     for cmd in ["makemigrations", "migrate", "compilemessages", "flush", "shell"]
@@ -229,4 +258,6 @@ def start():
     scheduler.add_job(update_experience, "interval", hours=4)
     scheduler.add_job(block_unblock_disciplinary, "interval", seconds=25)
     # scheduler.add_job(send_probation_end_notifications, "interval", days=1)
+    scheduler.add_job(send_contract_end_notification , "interval" , seconds=30)
+    scheduler.add_job(send_birthday_in_this_month_notification, "interval" , seconds=5)
     scheduler.start()
