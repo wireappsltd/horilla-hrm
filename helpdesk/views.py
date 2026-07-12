6aef2805650b0627f6d8c52a633fe576832ea195
Hasith Mendis <hasith@wireapps.co.uk>
2026-07-12 19:12:30 +0100

    HRMOD-550 - [Bug - Help Desk Change Request Form]Change Request Summary Is Displayed at the Bottom of the View Request Page]

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -5759,6 +5759,7 @@ def _build_change_request_description(change_request):
     """Render the Section 1 (Change Requester) summary as ticket description."""
     parts = [
         "<b>Change Request Details:</b><br><br>",
+        f"<b>Summary:</b> {strip_tags(change_request.summary)}<br>",
         f"<b>Categorisation:</b> {change_request.get_categorisation_display()}<br>",
         f"<b>Change Type:</b> {change_request.get_change_type_display()}<br>",
     ]
@@ -5768,8 +5769,7 @@ def _build_change_request_description(change_request):
         [
             f"<b>Services / Systems Impacted:</b> {strip_tags(change_request.services_impacted)}<br>",
             f"<b>Change Required By:</b> {change_request.change_required_by}<br>",
-            f"<b>Change Requested By:</b> {change_request.change_requested_by}<br>",
-            f"<b>Summary:</b> {strip_tags(change_request.summary)}",
+            f"<b>Change Requested By:</b> {change_request.change_requested_by}",
         ]
     )
     return "".join(parts)
